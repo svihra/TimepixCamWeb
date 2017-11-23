c40b6faf955d966dd24c02b62d178f2be32b451e
svihra <psvihra@gmail.com>
2017-11-23 19:39:28 +0100

    added visit counter

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,8 +17,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $toPeter = 'peter.svihra@cern.ch'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $toAndrei = 'anomerotski@bnl.gov'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "TpxCam Contact Form:  $name";
-$email_body = "You have received a new message from TpxCam website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "You have received a new message from TpxCam website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@tpxcam.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($toPeter,$email_subject,$email_body,$headers);
 #mail($toAndrei,$email_subject,$email_body,$headers);
