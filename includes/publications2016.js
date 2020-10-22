baf8e33d3e8ea149e38764a230ce0fa556675ed7
Peter Svihra <peter.svihra@cern.ch>
2020-10-22 17:38:06 +0200

    color ref change

--- includes/publications2016.js
+++ includes/publications2016.js
@@ -1,1 +1,1 @@
-document.write("<li id='TimeStamping'> M. Fisher-Levine and A. Nomerotski: <em>'TimepixCam: a fast optical imager with time-stamping'</em>. J. Instrum. 11 (2016) C03016. <a href='https://doi.org/10.1088/1748-0221/11/03/C03016' style="color: #ff000">DOI</a> </li>")
+document.write("<li id='TimeStamping'> M. Fisher-Levine and A. Nomerotski: <em>'TimepixCam: a fast optical imager with time-stamping'</em>. J. Instrum. 11 (2016) C03016. <a href='https://doi.org/10.1088/1748-0221/11/03/C03016' style='color: #ff000'>DOI</a> </li>")
